a2f63dbe9c1abaeb3f5057b8c0074900fe750b07
giandoso <a15029@bcc.unifal-mg.edu.br>
2020-10-08 22:56:47 -0300

    Como faremos as inscrições?

--- eventex/urls.py
+++ eventex/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-import eventex.core.views
+import eventex.core.views, eventex.subscriptions.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', eventex.core.views.home),
+    path('inscricao/', eventex.subscriptions.views.subscribe),
 ]
